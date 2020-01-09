be2f955b53f654208f36482cf253c8e35802e211
fooer <59675031+fooer@users.noreply.github.com>
2020-01-08 21:12:55 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,20 @@
-//Chad Oertel Game On!
+/*Chad Oertel Game On 3.0!
+ A first C++ program */
+
+//Preproccessor directive 
+#include <iostream>
+//two declarations for elements from the std namespace that i want local to my program 
+using std::cout;
+using std::endl;
+
+
+//main() Function integer meaning it will return a integer value 
+int main()
+//beginning of function
+{
+
+	// string lateral between the quotes sent thru cout, also using the declarations from the beginning?
+	cout << "Game On!" << endl;
+	//returns 0 to the operating system
+	return 0;
+	}
